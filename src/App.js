69c51179874f508e9aeba8860c16c27aa065067c
phpyazilim <phpyazilim@phpyazilim.com>
2019-03-31 01:36:26 +0300

    updated

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    turn: "X",
+    oyunSonu: false,
+    board: Array(9).fill(''),
+    toplamHareket:0,
+    kazanan:''
+  }
+
+  handleClick(event) {
+
+    if (this.state.oyunSonu==true) return;     
+
+    if (this.state.board[event.target.dataset.kare] == '') {
+      this.state.board[event.target.dataset.kare] = this.state.turn;
+
+       
+
+      event.target.innerText = this.state.turn;
+      this.setState({
+        turn: this.state.turn === 'X' ? 'O' : 'X',
+        board: this.state.board,
+        toplamHareket:++this.state.toplamHareket
+      })
+      
+    }
+
+    var sonuc = this.kazananKontrol();
+    if(sonuc=='X'){
+      this.setState({
+        oyunSonu:true,
+        kazanan:'X Kazandı'
+      })
+     
+      
+    } else if(sonuc=='O') {
+      this.setState({
+        oyunSonu:true,
+        kazanan:'O Kazandı'
+      })
+       
+    } else if(sonuc=='Beraberlik') {
+      this.setState({
+        oyunSonu:true,
+        kazanan:'Sonuç Beraberlik'
+      })
+    }
+
+  }
+
+
+  kazananKontrol() {
+    var board=this.state.board;
+    var kazanmaHareketleri=[[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6],[0,1,2],[3,4,5],[6,7,8]];
+
+    for(let i=0;i<kazanmaHareketleri.length;i++)
+    {
+      if(board[kazanmaHareketleri[i][0]]==board[kazanmaHareketleri[i][1]] &&  board[kazanmaHareketleri[i][1]]==board[kazanmaHareketleri[i][2]] )
+      {
+        return board[kazanmaHareketleri[i][0]];
+      }
+    
+    }
+
+     if(this.state.toplamHareket==9)
+     {
+       return 'Beraberlik';
+     }
+
+
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <div id="game">
+         
+          <div id="head">
+
+           Tic Tac Toe Game
+          </div>
+
+          <div id="board" onClick={(e) => this.handleClick(e)}>
+            <div className="kare" data-kare="0"></div>
+            <div className="kare" data-kare="1"></div>
+            <div className="kare" data-kare="2"></div>
+            <div className="kare" data-kare="3"></div>
+            <div className="kare" data-kare="4"></div>
+            <div className="kare" data-kare="5"></div>
+            <div className="kare" data-kare="6"></div>
+            <div className="kare" data-kare="7"></div>
+            <div className="kare" data-kare="8"></div>
+
+          </div>
+
+           <div id="durum">{this.state.kazanan}</div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
